f5d6cb765f71e774c05c1d0e29991bf1ce90c101
Ivan Boldyrev <lispnik@gmail.com>
2019-08-19 23:39:48 +0700

    Thread-local allocator

--- alloc.cpp
+++ alloc.cpp
@@ -1,9 +1,6 @@
 #include "alloc.hpp"
 #include <cassert>
-#include <stdexcept>
-#include <iostream>
-#include <iomanip>
-#include <unistd.h>
+#include <sys/mman.h>
 
 constexpr std::size_t DEFAULT_ALLOC_SIZE = 64 * 1024;
 constexpr size_t ALIGN_SIZE = 8;
@@ -13,13 +10,8 @@ static_assert(!(ALIGN_SIZE & (ALIGN_SIZE - 1)),
 static_assert(DEFAULT_ALLOC_SIZE % ALIGN_SIZE == 0,
               "DEFAULT_ALLOC_SIZE has to be aligned to ALIGN_SIZE'd");
 
-std::atomic<char*> MemorySingleton::free_end;
-std::atomic<char*> MemorySingleton::free_begin;
-std::atomic<bool> MemorySingleton::in_alloc;
-std::atomic<std::size_t> MemorySingleton::alloc_stat;
-std::atomic<std::size_t> MemorySingleton::sbrk_stat;
-
-void* last_sbrk; // Useful for debug, as core seems to keep no sbrk()
+thread_local char* free_end;
+thread_local char* free_begin;
 
 
 /**
@@ -47,75 +39,27 @@ static inline std::size_t AlignSize(std::size_t size) {
 }
 
 char* MemorySingleton::AllocSbrk(std::size_t size) {
-    bool in_alloc_expected = false;
+    // Name is misleading, we use mmap.
+    // sbrk is not thread-safe!!!
     size_t allocSize = SbrkAllocSize(size);
-    //std::cerr << "Sbrk size " << allocSize << " for " << size << std::endl;
-    if (in_alloc.compare_exchange_weak(in_alloc_expected, true)) {
-        char* sbrk_new = static_cast<char*>(last_sbrk = sbrk(allocSize));
-        sbrk_stat.fetch_add(allocSize);
-        if (sbrk_new == reinterpret_cast<void*>(-1)) {
-            throw std::runtime_error("OOM");
-        } else {
-            assert((((intptr_t)sbrk_new) & (ALIGN_SIZE - 1)) == 0);
-            // We have to update both begin and end together!!!
-            free_begin.store(sbrk_new + size);
-            // Now free_end < free_begin, no allocation in other
-            // thread can happen.
-            
-            // Updating end.
-            free_end.store(sbrk_new + allocSize);
-            in_alloc.store(false);
-            return sbrk_new;
-        }
-    } else {
-        return nullptr;
-    }
+    char* sbrk_new = static_cast<char*>(mmap(0, allocSize, PROT_READ|PROT_WRITE, MAP_PRIVATE|MAP_ANONYMOUS, -1, 0));
+    assert((((intptr_t)sbrk_new) & (ALIGN_SIZE - 1)) == 0);
+    free_begin = sbrk_new + size;
+    free_end = sbrk_new + allocSize;
+
+    return sbrk_new;
 }
 
 void* MemorySingleton::Allocate(std::size_t size) {
     size = AlignSize(size);
-    while (true) {
-        char* end = free_end.load();
-        // Order of fetching end and start is important 8-)>
-        // If AllocSbrk will happen before fetching end and start,
-        // end < start, and the loop will just restart immediately.
-        // If you change order of these statements, it will be
-        // start < end, but start and end will be from different
-        // memory regions...
-        char* start = free_begin.load();
+    char* start = free_begin;
+    char* end = free_end;
 
-        // It REALLY affects sbrk size!
-        if (end && start > end) {
-            continue;
-        }
-            
-        //std::cerr << (void*)start << " -> " << (void*)end << std::endl;
-        if (end && (start + size <= end)) {
-            /* ^ We check here for "end" because it is
-             * initialized/updated last */
-
-            char* new_start = start + size;
-            if (free_begin.compare_exchange_weak(start, new_start)) {
-                alloc_stat.fetch_add(size);
-                return start;
-            }
-            // else continue;
-        } else {
-            //std::cerr << "Try sbrk" << std::endl;
-            start = AllocSbrk(size);
-            
-            if (start) {
-                alloc_stat.fetch_add(size);
-                return start;
-            }
-            // else continue;
-        }
+    if (end && (start + size <= end)) {
+      char* new_start = start + size;
+      free_begin = new_start;
+      return start;
+    } else {
+      return AllocSbrk(size);
     }
-}
-
-
-void MemorySingleton::PrintStats() {
-    std::cerr << "sbrk size:  " << std::setw(18) << sbrk_stat.load() << std::endl
-              << "alloc size: " << std::setw(18) << alloc_stat.load() << std::endl
-              << "now free:   " << std::setw(18) << free_end.load() - free_begin.load() << std::endl;
 }
--- alloc.hpp
+++ alloc.hpp
@@ -1,22 +1,9 @@
-#include <atomic>
 #include <cstdint>
 
 class MemorySingleton {
-    static std::atomic<char*> free_end;
-    static std::atomic<char*> free_begin;
-    static std::atomic<bool> in_alloc;
-    static std::atomic<std::size_t> alloc_stat;
-    static std::atomic<std::size_t> sbrk_stat;
-
     static char* AllocSbrk(std::size_t size);
 public:
-    static void Init() {
-        free_end.store(nullptr);
-        free_begin.store(nullptr);
-        in_alloc.store(false);
-        alloc_stat.store(0);
-        sbrk_stat.store(0);
-    }
+    static void Init() { }
     static void* Allocate(std::size_t size);
-    static void PrintStats();
+    static void PrintStats() { }
 };
