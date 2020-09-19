c51dedfbff3fbd14e0658c77b99ba91b9cf31725
Ivan Boldyrev <lispnik@gmail.com>
2020-09-19 20:28:01 +0300

    Add config reading

---
+++ raft_experiment/src/config.rs
@@ -0,0 +1,38 @@
+use std::convert::From;
+use std::fs::File;
+use std::io;
+use serde_yaml;
+use serde::Deserialize;
+
+#[derive(Deserialize)]
+pub struct Config {
+    /// Client HTTP port
+    pub http_port: u16,
+    /// RAFT port for cluster intercommunication
+    pub raft_port: u16,
+    /// Initial list of nodes
+    pub nodes: Vec<String>,
+}
+
+#[derive(Debug)]
+pub enum Error {
+    IOError(io::Error),
+    YamlError(serde_yaml::Error),
+}
+
+impl From<io::Error> for Error {
+    fn from(err: io::Error) -> Self {
+        Error::IOError(err)
+    }
+}
+
+impl From<serde_yaml::Error> for Error {
+    fn from(err: serde_yaml::Error) -> Self {
+        Error::YamlError(err)
+    }
+}
+
+pub fn load_config(path: &str) -> Result<Config, Error> {
+    let mut file = File::open(path)?;
+    return Ok(serde_yaml::from_reader(&mut file)?);
+}
--- raft_experiment/src/main.rs
+++ raft_experiment/src/main.rs
@@ -1,3 +1,28 @@
+mod config;
+use clap;
+
 fn main() {
-    println!("Hello, world!");
+    let matches = clap::App::new("Simple RAFT experiment")
+        .version("1.1.1.1.1.1.1")
+        .author("monoid")
+        .arg(clap::Arg::with_name("config")
+             .long("config")
+             .required(true)
+             .takes_value(true))
+        .arg(clap::Arg::with_name("self")
+             .long("self")
+             .required(true)
+             .takes_value(true))
+        .get_matches();
+
+    let path = matches.value_of("config").expect("YAML config path expected");
+    let conf = config::load_config(&path).expect("Valid YAML config expected");
+    let node_self = matches.value_of("self").expect("self name expected");
+
+    eprintln!("HTTP port: {}, RAFT port: {}", conf.http_port, conf.raft_port);
+    eprintln!("Self: {}", node_self);
+    eprintln!("Nodes:");
+    for n in &conf.nodes {
+        eprintln!("{}", n);
+    }
 }
