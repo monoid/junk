eedd088b7017fb120acc9c1c85d892c5ce520c96
Ivan Boldyrev <lispnik@gmail.com>
2020-10-05 21:23:54 +0300

    Async log_storage

--- raft_experiment/src/log_storage.rs
+++ raft_experiment/src/log_storage.rs
@@ -1,24 +1,26 @@
-use std::fs;
-use std::io::{self, Read, Seek, Write};
+use std::io::SeekFrom;
 use std::path::Path;
-use byteorder::{NetworkEndian, ReadBytesExt, WriteBytesExt};
+use std::pin::Pin;
 
-type LogEndian = NetworkEndian;
+use async_trait::async_trait;
+use tokio::fs;
+use tokio::io::{self, AsyncWrite, AsyncReadExt, AsyncWriteExt};
 
+#[async_trait]
 pub trait Commiter {
-    // TODO async
     // TODO get_writer.  Should Commiter own the buffer and
     // write/sync by itself?
-    fn commit(&mut self, file: &mut fs::File) -> io::Result<()>;
+    async fn commit(&mut self, file: &mut fs::File) -> io::Result<()>;
 }
 
 pub struct NoopCommiter {
 }
 
-/// Noop commiter that does nothing.  Useful for unit tests where
+/// Noop commiter that does not sync.  Useful for unit tests where
 /// crashes are not tested (i.e. all unit tests).
+#[async_trait]
 impl Commiter for NoopCommiter {
-    fn commit(&mut self, _file: &mut fs::File) -> io::Result<()> {
+    async fn commit(&mut self, _file: &mut fs::File) -> io::Result<()> {
         Ok(())
     }
 }
@@ -28,10 +30,11 @@ impl Commiter for NoopCommiter {
 pub struct SyncDataCommiter {
 }
 
+#[async_trait]
 impl Commiter for SyncDataCommiter {
-    fn commit(&mut self, file: &mut fs::File) -> io::Result<()> {
-        file.flush()?;
-        file.sync_data()
+    async fn commit(&mut self, file: &mut fs::File) -> io::Result<()> {
+        file.flush().await?;
+        file.sync_data().await
     }
 }
 
@@ -53,15 +56,15 @@ impl<T> DoubleWAL<T> {
     /// Parse offsets_file, finding last commited data position.
     /// Truncate offset file and data file if incomplete or uncommited
     /// data is found.
-    pub fn new(mut data_file: fs::File, mut offsets_file: fs::File, commiter: T)
+    pub async fn new(mut data_file: fs::File, mut offsets_file: fs::File, commiter: T)
                -> io::Result<Self> {
         let mut data_committed_pos: u64 = 0;
         // We have learned the size, but move position to the end.
         // We get proper position from the offsets_file, and then
         // seek to it later (actually, this is a point of this method).
-        let data_len = data_file.seek(io::SeekFrom::End(0))?;
+        let data_len = data_file.seek(SeekFrom::End(0)).await?;
 
-        offsets_file.seek(io::SeekFrom::Start(0))?;
+        offsets_file.seek(SeekFrom::Start(0)).await?;
 
         // TODO refactor to a separate function to test it.  It needs
         // only Read to be tested.
@@ -71,7 +74,7 @@ impl<T> DoubleWAL<T> {
 
         // TODO add buffering only for reading.
         loop {
-            data_size = match offsets_file.read_u64::<LogEndian>() {
+            data_size = match offsets_file.read_u64().await {
                 Ok(n) => n,
                 Err(_) => {
                     break;
@@ -85,15 +88,15 @@ impl<T> DoubleWAL<T> {
                 break;
             }
         }
-        offsets_file.seek(io::SeekFrom::Start(offset_offset))?;
+        offsets_file.seek(SeekFrom::Start(offset_offset)).await?;
         // TODO: log offsets truncation.
-        offsets_file.set_len(offset_offset)?;
-        offsets_file.sync_data()?;
+        offsets_file.set_len(offset_offset).await?;
+        offsets_file.sync_data().await?;
 
-        data_file.seek(io::SeekFrom::Start(data_committed_pos))?;
+        data_file.seek(SeekFrom::Start(data_committed_pos)).await?;
         // TODO: log data truncation.
-        data_file.set_len(data_committed_pos)?;
-        data_file.sync_data()?;
+        data_file.set_len(data_committed_pos).await?;
+        data_file.sync_data().await?;
 
         Ok(Self {
             data_file,
@@ -102,7 +105,7 @@ impl<T> DoubleWAL<T> {
         })
     }
 
-    pub fn open<P: AsRef<Path>>(data_path: P, offsets_path: P, commiter: T)
+    pub async fn open<P: AsRef<Path>>(data_path: P, offsets_path: P, commiter: T)
                                 -> io::Result<Self> {
         let log_options = {
             let mut log_options = fs::OpenOptions::new();
@@ -110,14 +113,14 @@ impl<T> DoubleWAL<T> {
             log_options
         };
         // TODO advisory lock?
-        let data_file = log_options.open(data_path)?;
-        let offsets_file = log_options.open(offsets_path)?;
-        Self::new(data_file, offsets_file, commiter)
+        let data_file = log_options.open(data_path).await?;
+        let offsets_file = log_options.open(offsets_path).await?;
+        Self::new(data_file, offsets_file, commiter).await
     }
 
-    pub fn get_writer(&mut self) -> io::Result<DoubleWALWriter<T>> {
-        let data_rollback_pos = self.data_file.seek(io::SeekFrom::Current(0))?;
-        let offset_rollback_pos = self.offsets_file.seek(io::SeekFrom::Current(0))?;
+    pub async fn get_writer<'a>(&'a mut self) -> io::Result<DoubleWALWriter<'a, T>> {
+        let data_rollback_pos = self.data_file.seek(SeekFrom::Current(0)).await?;
+        let offset_rollback_pos = self.offsets_file.seek(SeekFrom::Current(0)).await?;
 
         Ok(DoubleWALWriter {
             data_rollback_pos,
@@ -128,18 +131,18 @@ impl<T> DoubleWAL<T> {
 }
 
 impl<T> DoubleWALWriter<'_, T> where T: Commiter {
-    pub fn abort(&mut self) -> io::Result<()> {
+    pub async fn abort(&mut self) -> io::Result<()> {
         let parent = self.parent.take().expect("abort is called on destructed DoubleWALWriter");
-        parent.offsets_file.set_len(self.offset_rollback_pos)?;
-        parent.data_file.set_len(self.data_rollback_pos)
+        parent.offsets_file.set_len(self.offset_rollback_pos).await?;
+        parent.data_file.set_len(self.data_rollback_pos).await
     }
 
-    pub fn commit(&mut self) -> io::Result<()> {
+    pub async fn commit(&mut self) -> io::Result<()> {
         let parent = self.parent.take().expect("commit is called on destructed DoubleWALWriter");
-        parent.commiter.commit(&mut parent.data_file)?;
-        let data_new_pos = parent.data_file.seek(io::SeekFrom::Current(0))?;
-        parent.offsets_file.write(&(data_new_pos - self.data_rollback_pos).to_ne_bytes())?;
-        parent.commiter.commit(&mut parent.offsets_file)
+        parent.commiter.commit(&mut parent.data_file).await?;
+        let data_new_pos = parent.data_file.seek(SeekFrom::Current(0)).await?;
+        parent.offsets_file.write_u64(data_new_pos - self.data_rollback_pos).await?;
+        parent.commiter.commit(&mut parent.offsets_file).await
     }
 }
 
@@ -154,12 +157,20 @@ impl<T> Drop for DoubleWALWriter<'_, T> {
     }
 }
 
-impl<'a, T> io::Write for DoubleWALWriter<'a, T> {
-    fn write(&mut self, buf: &[u8]) -> io::Result<usize> {
-        self.parent.as_mut().unwrap().data_file.write(buf)
+impl<'a, T> AsyncWrite for DoubleWALWriter<'a, T> {
+    fn poll_write(
+        mut self: std::pin::Pin<&mut Self>,
+        cx: &mut std::task::Context<'_>,
+        buf: &[u8],
+    ) -> std::task::Poll<Result<usize, io::Error>> {
+        Pin::new(&mut self.parent.as_mut().unwrap().data_file).poll_write(cx, buf)
     }
-    
-    fn flush(&mut self) -> io::Result<()> {
-        self.parent.as_mut().unwrap().data_file.flush()
+
+    fn poll_flush(mut self: std::pin::Pin<&mut Self>, cx: &mut std::task::Context<'_>) -> std::task::Poll<Result<(), io::Error>> {
+        Pin::new(&mut self.parent.as_mut().unwrap().data_file).poll_flush(cx)
+    }
+
+    fn poll_shutdown(mut self: std::pin::Pin<&mut Self>, cx: &mut std::task::Context<'_>) -> std::task::Poll<Result<(), io::Error>> {
+        Pin::new(&mut self.parent.as_mut().unwrap().data_file).poll_shutdown(cx)
     }
 }
