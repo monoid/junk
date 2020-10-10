e4b2d0a852d64ffbcce098a316378275c46afaec
Ivan Boldyrev <lispnik@gmail.com>
2020-10-11 00:25:22 +0300

    Another liblog sketch

--- raft_experiment/liblog/src/storage.rs
+++ raft_experiment/liblog/src/storage.rs
@@ -5,60 +5,91 @@ use std::pin::Pin;
 
 use async_trait::async_trait;
 use tokio::fs;
-use tokio::io::{self, AsyncWrite, AsyncReadExt, AsyncWriteExt};
+use tokio::io::{self, AsyncReadExt, AsyncWrite};
 
-#[async_trait]
-pub trait Commiter {
-    // TODO get_writer.  Should Commiter own the buffer and
-    // write/sync by itself?
-    async fn commit(&mut self, file: &mut fs::File) -> io::Result<()>;
-}
+/**
+ * Raft request (i.e. command) Log.
+ *
+ * LogWriter implements writing strategy.  Writing after each record,
+ * sync after several records/on timeout.
+ *
+ * AsyncWAL incapsulates writing and syncing method, be it memory,
+ * files without sync, files+f(data)sync, DIRECT_IO etc.
+ *
+ */
 
-pub struct NoopCommiter {
-}
+/// Safety wrapper to hide implementation (e.g. to hide file methods).
+/// You can use it as any AsyncWrite.
+pub struct AsyncWriteWrapper<'a, T: AsyncWrite + Send + Sync>(&'a mut T);
 
-/// Noop commiter that does not sync.  Useful for unit tests where
-/// crashes are not tested (i.e. all unit tests).
-#[async_trait]
-impl Commiter for NoopCommiter {
-    async fn commit(&mut self, _file: &mut fs::File) -> io::Result<()> {
-        Ok(())
+impl<'a, T: AsyncWrite + Send + Sync + Unpin> AsyncWrite for AsyncWriteWrapper<'a, T> {
+    fn poll_write(
+        mut self: std::pin::Pin<&mut Self>,
+        cx: &mut std::task::Context<'_>,
+        buf: &[u8],
+    ) -> std::task::Poll<Result<usize, io::Error>> {
+        Pin::new(&mut self.0).poll_write(cx, buf)
+    }
+
+    fn poll_flush(
+        mut self: std::pin::Pin<&mut Self>,
+        cx: &mut std::task::Context<'_>,
+    ) -> std::task::Poll<Result<(), io::Error>> {
+        Pin::new(&mut self.0).poll_flush(cx)
+    }
+
+    fn poll_shutdown(
+        mut self: std::pin::Pin<&mut Self>,
+        cx: &mut std::task::Context<'_>,
+    ) -> std::task::Poll<Result<(), io::Error>> {
+        Pin::new(&mut self.0).poll_shutdown(cx)
     }
 }
 
-/// Commiter with `File::sync_data(...)`.  We do not rely on timestamps,
-/// thus `sync_data` aka `fsyncdata(2)` is enough for us.
-pub struct SyncDataCommiter {
-}
 
-#[async_trait]
-impl Commiter for SyncDataCommiter {
-    async fn commit(&mut self, file: &mut fs::File) -> io::Result<()> {
-        file.flush().await?;
-        file.sync_data().await
-    }
-}
-
-trait AsyncWriteSync: AsyncWrite + Send {
-}
-
-impl<T: AsyncWrite + Send> AsyncWriteSync for T {
-}
-
-
+/// Async log writer.  The write_command async method gets an FnOnce that can
+/// write data, and all written data will be recorded as log command.  Its size and
+/// position will be durably recorded as well.
+///
+/// Please note that LogWriter can group writes into batches in data
+/// size and time manner, and command will return on when
+/// requested data is durably written or error is detected.  See
+/// documentation for particular implementation.  Of course, order of
+/// commands is respected.
 #[async_trait]
 trait LogWriter {
-    type W: AsyncWriteSync;
+    type W: AsyncWrite + Send + Sync;
     /// Write data to the log, committing it after that.  serializer
-    /// is an async function that gets a writer and outputs data,
+    /// is an async function that gets an AsyncWrite to write data,
     /// e.g. with tokio-serde.
-    async fn writer_record<I, T, F>(
-        &'async_trait mut self,  // TODO Pin?
+    async fn command<I, T, F>(
+        &'async_trait mut self, // TODO Pin?
         serializer: I,
     ) -> F::Output
     where
-        I: Fn(&'async_trait mut Self::W) -> F + Send + Sync,
-        F: Future<Output=io::Result<T>> + Send + Sync;
+        I: Fn(AsyncWriteWrapper<'async_trait, Self::W>) -> F + Send + Sync,
+        F: Future<Output = io::Result<T>> + Send + Sync;
+}
+
+/// WAL aka log.
+// TODO rename to storage?
+pub trait AsyncWAL {
+    type W: AsyncWrite + Send + Sync;
+    type CommandPos: Send + Sync;
+    // TODO: command that returns (CommandPos, F::Output), like LogWriter.
+    // TODO: commit_data -- it seems the only reasonable way to make nocommit log is create nocommit
+    //       AsyncWAL.  Thus it is indices who will commit first commands, then indices.
+    //       And commit_methods should not exist.
+    // TODO: indices(&[CommandPos])
+    // TODO: commit_indices
+    fn get_data_writer(&mut self) -> &mut Self::W;
+}
+
+/// Stupid writer that doesn't bother with syncing at all.
+/// Useful for non-durability tests.
+pub struct NoopLogWriter<Wal> {
+    // TODO: abstract WAL: memory, normal files, direct write
+    wal: Wal,
 }
 
 /// Simple log with data and index files.
@@ -67,19 +98,11 @@ struct SimpleWAL {
     offsets_file: fs::File,
 }
 
-/// Stupid writer that doesn't bother with syncing at all.
-/// Useful for non-durability tests.
-pub struct NoopLogWriter {
-    // TODO: abstract WAL: normal files, direct write 
-    wal: SimpleWAL,
-}
-
 impl SimpleWAL {
     /// Parse offsets_file, finding last commited data position.
     /// Truncate offset file and data file if incomplete or uncommited
     /// data is found.
-    pub async fn new(mut data_file: fs::File, mut offsets_file: fs::File)
-               -> io::Result<Self> {
+    pub async fn new(mut data_file: fs::File, mut offsets_file: fs::File) -> io::Result<Self> {
         let mut data_committed_pos: u64 = 0;
         // We have learned the size, but move position to the end.
         // We get proper position from the offsets_file, and then
@@ -126,8 +149,7 @@ impl SimpleWAL {
         })
     }
 
-    pub async fn open<P: AsRef<Path>>(data_path: P, offsets_path: P)
-                                      -> io::Result<Self> {
+    pub async fn open<P: AsRef<Path>>(data_path: P, offsets_path: P) -> io::Result<Self> {
         let log_options = {
             let mut log_options = fs::OpenOptions::new();
             log_options.read(true).write(true).create(true);
@@ -141,82 +163,55 @@ impl SimpleWAL {
 }
 
 #[async_trait]
-impl LogWriter for NoopLogWriter {
-    type W = fs::File;
-    async fn writer_record<I, T, F>(
-        &'async_trait mut self,  // TODO Pin?
+impl<TWal: AsyncWAL + Send + Sync> LogWriter for NoopLogWriter<TWal> {
+    type W = TWal::W;
+    async fn command<I, T, F>(
+        &'async_trait mut self, // TODO Pin?
         serializer: I,
     ) -> F::Output
     where
-        I: Fn(&'async_trait mut Self::W) -> F + Send + Sync,
-        F: Future<Output=io::Result<T>> + Send + Sync
+        I: Fn(AsyncWriteWrapper<'async_trait, Self::W>) -> F + Send + Sync,
+        F: Future<Output = io::Result<T>> + Send + Sync,
     {
         // TODO it should handle multiple records.
         // It seems WAL should contain queue of record indices
         // let recidx = self.wal.TODO_begin_record()?;
-        let val = serializer(&mut self.wal.data_file).await?;
+        let val = serializer(AsyncWriteWrapper(self.wal.get_data_writer())).await?;
         // recidx.TODO_finish()?;
-        // recidx.flush_all(); 
+        // recidx.flush_all();
         Ok(val)
     }
 }
 
-pub struct DoubleWAL<T> {
-    data_file: fs::File,
-    offsets_file: fs::File,
-    commiter: T,
-}
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use tokio::io::AsyncWriteExt;
 
+    #[tokio::test(threaded_scheduler)]
+    async fn test_async_write_simple() -> io::Result<()> {
+        // A simple test that proofs that whole idea is implementable.
+        struct MemWal {
+            data: Vec<u8>,
+        }
 
-pub struct DoubleWALWriter<'a, T> {
-    data_rollback_pos: u64,
-    offset_rollback_pos: u64,
-    parent: Option<&'a mut DoubleWAL<T>>,
-}
+        impl AsyncWAL for MemWal {
+            type W = Vec<u8>;
+            type CommandPos = ();
 
-impl<T> DoubleWALWriter<'_, T> where T: Commiter {
-    // TODO what is the point of the abort?  Transaction abort is an extra record, not
-    // log abort.
-    pub async fn abort(&mut self) -> io::Result<()> {
-        let parent = self.parent.take().expect("abort is called on destructed DoubleWALWriter");
-        parent.offsets_file.set_len(self.offset_rollback_pos).await?;
-        parent.data_file.set_len(self.data_rollback_pos).await
-    }
-
-    pub async fn commit(&mut self) -> io::Result<()> {
-        let parent = self.parent.take().expect("commit is called on destructed DoubleWALWriter");
-        parent.commiter.commit(&mut parent.data_file).await?;
-        let data_new_pos = parent.data_file.seek(SeekFrom::Current(0)).await?;
-        parent.offsets_file.write_u64(data_new_pos - self.data_rollback_pos).await?;
-        parent.commiter.commit(&mut parent.offsets_file).await
-    }
-}
-
-impl<T> Drop for DoubleWALWriter<'_, T> {
-    fn drop(&mut self) {
-        match self.parent {
-            None => {},
-            Some(_) => {
-                panic!("Call either abort or commit on the DoubleWALWriter");
+            fn get_data_writer(&mut self) -> &mut Self::W {
+                &mut self.data
             }
         }
-    }
-}
 
-impl<'a, T> AsyncWrite for DoubleWALWriter<'a, T> {
-    fn poll_write(
-        mut self: std::pin::Pin<&mut Self>,
-        cx: &mut std::task::Context<'_>,
-        buf: &[u8],
-    ) -> std::task::Poll<Result<usize, io::Error>> {
-        Pin::new(&mut self.parent.as_mut().unwrap().data_file).poll_write(cx, buf)
-    }
+        let mut noop = NoopLogWriter {
+            wal: MemWal {
+                data: Default::default(),
+            },
+        };
 
-    fn poll_flush(mut self: std::pin::Pin<&mut Self>, cx: &mut std::task::Context<'_>) -> std::task::Poll<Result<(), io::Error>> {
-        Pin::new(&mut self.parent.as_mut().unwrap().data_file).poll_flush(cx)
-    }
-
-    fn poll_shutdown(mut self: std::pin::Pin<&mut Self>, cx: &mut std::task::Context<'_>) -> std::task::Poll<Result<(), io::Error>> {
-        Pin::new(&mut self.parent.as_mut().unwrap().data_file).poll_shutdown(cx)
+        noop.command(|mut w| async move { w.write_all("Hello!".as_bytes()).await }).await?;
+        assert!(String::from_utf8(noop.wal.data) == Ok("Hello!".to_string()));
+        Ok(())
     }
 }
