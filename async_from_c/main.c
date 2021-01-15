8d1a7d9b31f3fe0860778b7fbf1d013b1cc4d00d
Ivan Boldyrev <lispnik@gmail.com>
2021-01-15 12:49:33 +0300

    async from c sample

---
+++ async_from_c/main.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <string.h>
+
+extern char* query(const char* url);
+extern void free_result(char* res);
+
+int main() {
+  char *r = query("https://ya.ru");
+  if (r) {
+    printf("%lu\n", strlen(r));
+  } else {
+    printf("no data\n");
+  }
+  free_result(r);
+
+  r = query("https://www.google.com");
+  if (r) {
+    printf("%lu\n", strlen(r));
+  } else {
+    printf("no data\n");
+  }
+  free_result(r);
+
+  return 0;
+}
---
+++ async_from_c/src/lib.rs
@@ -0,0 +1,69 @@
+use std::os::raw::c_char;
+use std::ffi::{CStr, CString};
+
+use once_cell::sync::Lazy;
+use tokio_stream::StreamExt;
+
+static RUNTIME: Lazy<tokio::runtime::Runtime> = Lazy::new(|| {
+    tokio::runtime::Builder::new_multi_thread()
+        .enable_all()
+        .build()
+        .unwrap()
+});
+
+#[no_mangle]
+extern "C"
+fn query(url: *const c_char) -> *mut c_char {
+    match unsafe { CStr::from_ptr(url).to_str() } {
+        Ok(url) => {
+            let data = RUNTIME.block_on(async {
+                let mut data = vec![0u8; 0];
+                match reqwest::get(url).await {
+                    Ok(res) => {
+                        let mut stream = res.bytes_stream();
+                        while let Some(chunk) = stream.next().await {
+                            match chunk {
+                                Ok(chunk) => {
+                                    data.extend_from_slice(&chunk[..]);
+                                },
+                                Err(_) => {
+                                    return None;
+                                }
+                            }
+                        }
+                    },
+                    Err(_) => {
+                        return None;
+                    }
+                }
+                Some(data)
+            });
+            match data.and_then(|d| CString::new(d).ok()) {
+                Some(data) => {
+                    data.into_raw()
+                }
+                None => {
+                    std::ptr::null_mut()
+                }
+            }
+        },
+        Err(_) => {
+            std::ptr::null_mut()
+        }
+    }
+}
+
+#[no_mangle]
+extern "C" fn free_result(data: *mut c_char) {
+    unsafe {
+        CString::from_raw(data)
+    };
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn it_works() {
+        assert_eq!(2 + 2, 4);
+    }
+}
