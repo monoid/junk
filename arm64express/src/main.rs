e1c8a1b5a662f333bfcffe15475270e89366046b
Ivan Boldyrev <lispnik@gmail.com>
2023-05-25 15:52:57 +0200

    arm64express: experimental dynasmrt code call

--- arm64express/src/main.rs
+++ arm64express/src/main.rs
@@ -1,5 +1,8 @@
 mod ast;
+mod codegen;
 mod parser;
+
+use crate::codegen::Function;
 
 fn main() {
     let ast = parser::parse("(((5)+42)*8)").unwrap().1;
@@ -7,4 +10,7 @@ fn main() {
 
     let ast = parser::parse("8-2-2").unwrap().1;
     println!("{}", ast.eval(&Default::default()).unwrap());
+
+    let func = Function::new();
+    println!("{}", func.call(80, 1));
 }
