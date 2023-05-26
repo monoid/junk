cf469afab1523f8cdb7b16aa52f69a75de2b27e0
Ivan Boldyrev <lispnik@gmail.com>
2023-05-26 12:05:49 +0200

    arm64express: Rename `Ast::Const` to `Ast::Literal`

--- arm64express/src/ast.rs
+++ arm64express/src/ast.rs
@@ -4,7 +4,7 @@ pub(crate) type Var = String;
 pub(crate) type Val = i32;
 
 pub(crate) enum Ast {
-    Const(Val),
+    Literal(Val),
     Var(Var),
     Add(Box<Ast>, Box<Ast>),
     Sub(Box<Ast>, Box<Ast>),
@@ -14,7 +14,7 @@ pub(crate) enum Ast {
 
 impl Ast {
     pub(crate) fn literal(val: Val) -> Self {
-        Self::Const(val)
+        Self::Literal(val)
     }
 
     pub(crate) fn var(var: Var) -> Self {
@@ -39,7 +39,7 @@ impl Ast {
 
     pub(crate) fn eval(&self, env: &HashMap<Var, Val>) -> Result<Val, Box<dyn std::error::Error>> {
         match self {
-            Ast::Const(val) => Ok(*val),
+            Ast::Literal(val) => Ok(*val),
             Ast::Var(name) => env.get(name).copied().ok_or("A variable not found".into()),
             Ast::Add(a1, a2) => Ok(a1.eval(env)? + a2.eval(env)?),
             Ast::Sub(a1, a2) => Ok(a1.eval(env)? - a2.eval(env)?),
